8137afd66ddc64975114dd818d9544a30fcafe2c
abhishek <abhishek.goel287@gmail.com>
2024-08-23 13:31:12 +0530

    latest

--- gasFee.js
+++ gasFee.js
@@ -1,36 +1,38 @@
-import { Api, TonApiClient } from "@ton-api/client";
-import { WalletContractV4, internal, TonClient } from "@ton/ton";
-import { Address, toNano, SendMode ,beginCell} from "@ton/core";
+import { TonClient } from "@ton/ton";
+import { Address, toNano, SendMode, beginCell } from "@ton/core";
 import { mnemonicToPrivateKey } from "@ton/crypto";
-import { ContractAdapter } from "@ton-api/ton-adapter";
+import { WalletContractV4 } from "@ton/ton";
 
-// const endpoint = await getHttpEndpoint({ network: "testnet" });
 const client = new TonClient({
   endpoint: "https://testnet.toncenter.com/api/v2/jsonRPC",
   apiKey: "4aaa9ba4d04b1f5d88245bcecef4d0723f36ba95b225e4c5a1c1351e55dd53ce",
 });
 
+const createWallet = async (mnemonic) => {
+  const keyPair = await mnemonicToPrivateKey(mnemonic.split(" "));
+  const wallet = WalletContractV4.create({
+    workchain: 0,
+    publicKey: keyPair.publicKey,
+  });
+  return { wallet, keyPair };
+};
 
-export const gasFee = async (walletAddress) => {;
-  const key=process.env.KEY
+const sendTransaction = async (contract, params) => {
+  const seqno = await contract.getSeqno();
+  const txn = contract.createTransfer({ ...params, seqno });
+  return contract.sendTransfer(params);
+};
+
+export const gasFee = async (walletAddress) => {
+  const key = process.env.KEY;
   try {
-    const mnemonic =key
-    const keyPair = await mnemonicToPrivateKey(mnemonic.split(" "));
-    const workChain = 0;
-
-    const wallet = WalletContractV4.create({
-      workchain: workChain,
-      publicKey: keyPair.publicKey,
-    });
-
+    const { wallet, keyPair } = await createWallet(key);
     const contract = client.open(wallet);
 
     console.log(contract.address);
     console.log(await contract.getBalance());
-    const seqno = await contract.getSeqno();
 
     const params = {
-      seqno,
       secretKey: keyPair.secretKey,
       messages: [
         internal({
@@ -41,21 +43,16 @@ export const gasFee = async (walletAddress) => {;
       ],
     };
 
-    const txn = contract.createTransfer(params);
-    const tetherTransferForSend = await contract.sendTransfer(params);
-
+    const tetherTransferForSend = await sendTransaction(contract, params);
     console.log("A gasless transfer sent!", tetherTransferForSend);
-    if(txn){
-      return true
-    }
-    return false
+    return true;
   } catch (error) {
     console.error("An error occurred:", error);
-   
     console.log(error.body);
     if (error instanceof RangeError) {
       console.error("A RangeError occurred:", error);
     }
+    return false;
   }
 };
 
@@ -65,49 +62,42 @@ export const createJettonTransferTransaction = async (
   jettonMasterAddress
 ) => {
   try {
-    // Create a message to transfer jettons
     const forwardPayload = beginCell()
-        .storeUint(0, 32) // 0 opcode means we have a comment
-        .storeStringTail('Hello, TON!')
-        .endCell();
+      .storeUint(0, 32)
+      .storeStringTail("Hello, TON!")
+      .endCell();
 
     const jettonTransferMessage = {
-      to: Address.parse(jettonMasterAddress), // Jetton master contract address
-      value: toNano("0.2"), // Attach some TON for gas fees
+      to: Address.parse(jettonMasterAddress),
+      value: toNano("0.2"),
       body: beginCell()
-      .storeUint(0x0f8a7ea5, 32) // opcode for jetton transfer
-      .storeUint(0, 64) // query id
-      .storeCoins(toNano(500)) // jetton amount, amount * 10^9
-      .storeAddress(Address.parse("UQDkkpOBxvbbaTtQUTT25fTR39pqXFtA3BNH5Z7e7Twrc_ik"))
-      .storeAddress(Address.parse("UQDkkpOBxvbbaTtQUTT25fTR39pqXFtA3BNH5Z7e7Twrc_ik")) // response destination
-      .storeBit(0) // no custom payload
-      .storeCoins(toNano('0.1')) // forward amount - if >0, will send notification message
-      .storeBit(1) // we store forwardPayload as a reference
-      // .storeRef(forwardPayload)
-      .endCell()
+        .storeUint(0x0f8a7ea5, 32)
+        .storeUint(0, 64)
+        .storeCoins(toNano(500))
+        .storeAddress(
+          Address.parse("UQDkkpOBxvbbaTtQUTT25fTR39pqXFtA3BNH5Z7e7Twrc_ik")
+        )
+        .storeAddress(
+          Address.parse("UQDkkpOBxvbbaTtQUTT25fTR39pqXFtA3BNH5Z7e7Twrc_ik")
+        )
+        .storeBit(0)
+        .storeCoins(toNano("0.1"))
+        .storeBit(1)
+        .endCell(),
     };
-    
-    // Create an unsigned transaction
+
     const unsignedTransaction = {
       to: jettonMasterAddress,
       value: toNano("0.05").toString(),
       body: jettonTransferMessage.body.toBoc().toString("base64"),
     };
-    // const responseData = {
-    //   to: unsignedTransaction.to, // Convert Address to string
-    //   value: unsignedTransaction.value.toString(), // Convert BigInt to string
-    //   body: unsignedTransaction.body.toBoc().toString('base64'), // Convert Cell to base64 string
-    // };
 
-    // Return as JSON
-    // return JSON.stringify(unsignedTransaction);
     return {
       unsignedTransaction,
       userWalletAddress,
-    }
+    };
   } catch (error) {
     console.error("An error occurred:", error);
-    throw error; 
+    throw error;
   }
 };
-
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import axios from "axios";
 import { TonClient, Address, address } from "@ton/ton";
-import dotenv from 'dotenv';
-import { gasFee , createJettonTransferTransaction} from "./gasFee.js";
+import dotenv from "dotenv";
+import { gasFee, createJettonTransferTransaction } from "./gasFee.js";
 import cors from "cors";
 dotenv.config();
 
 const app = express();
 const port = 3000;
 
-app.use(cors())
+app.use(cors());
 
 const client = new TonClient({
   endpoint: "https://testnet.toncenter.com/api/v2/jsonRPC",
@@ -34,67 +34,71 @@ function nanotonsToTon(nanotons) {
   return (Number(nanotons) / 1e9).toFixed(9);
 }
 
+app.post("/swap", async (req, res) => {
+  const { walletAddress, tokenAddress, amount } = req.body;
 
+  console.log(req.body);
+  const claim = [];
 
-app.post('/swap', async (req, res) => {
-    const { walletAddress, tokenAddress, amount } = req.body;
-    
-    console.log(req.body)
-    const claim=[]
-  
-    if (!walletAddress || !tokenAddress || !amount) {
-      return res.status(400).send('Missing required parameters');
-    }
-    const userIndex = claim.findIndex(user => user.address === walletAddress);
+  if (!walletAddress || !tokenAddress || !amount) {
+    return res.status(400).send("Missing required parameters");
+  }
+  const userIndex = claim.findIndex((user) => user.address === walletAddress);
 
-    if (userIndex !== -1) {
-        if (claim[userIndex].limit >= 3) {
-        } else {
-            claim[userIndex].limit += 1;
-        }
+  if (userIndex !== -1) {
+    if (claim[userIndex].limit >= 3) {
     } else {
-        claim.push({
-            address: walletAddress,
-            limit: 1
-        });
+      claim[userIndex].limit += 1;
     }
-    try {
-      // Fetch jetton balances and wallet balance
-      const jettonBalances = await getJettonBalances(walletAddress);
-      // const selectedToken = jettonBalances.find(
-      //   (token) => Address.parse(token.jetton.address).toString()=== tokenAddress
-      // );
-      
-      // if (!selectedToken) {
-      //   return res.status(404).send('Token not present');
-      // }
-  
-      const walletBalance = await client.getBalance(walletAddress);
-      const balanceInTon = nanotonsToTon(walletBalance.toString());
-        // if(balanceInTon>0){
-      //   return res.status(404).send('not eligible');
-      // }
+  } else {
+    claim.push({
+      address: walletAddress,
+      limit: 1,
+    });
+  }
+  try {
+    // Fetch jetton balances and wallet balance
+    const jettonBalances = await getJettonBalances(walletAddress);
 
-       const con=await gasFee(walletAddress)
-       console.log("con" , con)
-      if( con){
-        const token =Address.parse(tokenAddress).toString()
-        const data = await createJettonTransferTransaction(walletAddress , amount , token)
-        console.log("transac data" , data)
-        return res.status(200).json({success:true , data:data});
-      }
+    console.log(jettonBalances);
 
-  
-      res.json(selectedToken);
-  
-    } catch (error) {
-      console.error(`Error in /swap endpoint: ${error.message}`);
-      res.status(500).send('Internal server error');
+    const selectedToken = jettonBalances.find(
+      (token) =>
+        Address.parse(token.jetton.address).toString() ===
+        Address.parse(tokenAddress).toString()
+    );
+
+    if (!selectedToken) {
+      return res.status(404).send("Token not present");
     }
-  });
+
+    // const walletBalance = await client.getBalance(walletAddress);
+    // const balanceInTon = nanotonsToTon(walletBalance.toString());
+    // if (balanceInTon > 0) {
+    //   return res.status(404).send("not eligible");
+    // }
+
+    // const con = await gasFee(walletAddress);
+    const con = true;
+
+    if (con) {
+      const token = Address.parse(tokenAddress).toString();
+      const data = await createJettonTransferTransaction(
+        walletAddress,
+        amount,
+        token
+      );
+      console.log("transac data", data);
+      return res.status(200).json({ success: true, data: data });
+    }
+
+    res.json(selectedToken);
+  } catch (error) {
+    console.error(`Error in /swap endpoint: ${error.message}`);
+    res.status(500).send("Internal server error");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
-
